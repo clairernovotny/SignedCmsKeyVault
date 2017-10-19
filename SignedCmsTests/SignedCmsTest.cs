0feb062dc442ecd5ba7b3785242d3baf7be3773f
Oren Novotny <oren@novotny.org>
2017-10-19 12:18:55 -0400

    Round trip and interop with BC

--- SignedCmsTests/SignedCmsTest.cs
+++ SignedCmsTests/SignedCmsTest.cs
@@ -1,8 +1,14 @@
 using System;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Security.Cryptography.Pkcs;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using Microsoft.Build.Utilities;
+using Org.BouncyCastle.Cms;
+using Org.BouncyCastle.Crypto.Operators;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.X509.Store;
 using Xunit;
 
 namespace SignedCmsTests
@@ -41,6 +47,56 @@ namespace SignedCmsTests
             var str = Encoding.UTF8.GetString(cContent);
 
             Assert.Equal(content, str);
+        }
+
+        [ConfigFact]
+        public void SignedCmsRoundTripWithBouncyCastleLocalCertificate()
+        {
+            var content = "This is some content";
+
+            // Get cert
+            
+            var netcert = GetLocalSignerCert();
+
+            var chain = new X509Chain();
+            chain.Build(netcert);
+            
+            // Get the chain without the root CA
+            var additionals = chain.ChainElements.Cast<X509ChainElement>()
+                .Where(ce => ce.Certificate.Issuer != ce.Certificate.SubjectName.Name)
+                .Select(ce => DotNetUtilities.FromX509Certificate(ce.Certificate))
+                .ToList();
+
+            chain.Dispose();
+            
+
+            var bcCer = DotNetUtilities.FromX509Certificate(netcert);
+            var bcKey = DotNetUtilities.GetRsaKeyPair(netcert.GetRSAPrivateKey());
+
+            var store = X509StoreFactory.Create("Certificate/Collection", new X509CollectionStoreParameters(additionals));
+
+            var generator = new CmsSignedDataGenerator();
+            var builder =  new SignerInfoGeneratorBuilder();
+
+            generator.AddSigner(bcKey.Private, bcCer, CmsSignedDataGenerator.DigestSha256);
+            generator.AddCertificates(store);
+
+            var msg = new CmsProcessableByteArray(Encoding.UTF8.GetBytes(content));
+            var data = generator.Generate(msg, true);
+
+            
+            var encoded = data.GetEncoded();
+
+
+            var signedCms = new SignedCms();
+            signedCms.Decode(encoded);
+            signedCms.CheckSignature(true); // don't validate the certiciate itself here
+
+            var cContent = signedCms.ContentInfo.Content;
+            var str = Encoding.UTF8.GetString(cContent);
+
+            Assert.Equal(content, str);
+
         }
 
         static public X509Certificate2 GetLocalSignerCert()
